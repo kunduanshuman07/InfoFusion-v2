1ccdf08a113ad77bace0d8a729c23c629e38c716
Anshuman Kundu <kundu4coding@gmail.com>
2024-04-14 00:06:45 +0530

    Scorecards Fixed

--- frontend-2.0/app/scorecards/[id]/page.tsx
+++ frontend-2.0/app/scorecards/[id]/page.tsx
@@ -17,37 +17,44 @@ const ScorecardsTable = () => {
         }
         fetchScorecardData();
     }, [id])
-    console.log(scorecardsData);
     return (
-        <div className="overflow-x-auto">
-            <table className="table">
-                <thead>
-                    <tr className="bg-cyan-800 text-white text-xs font-bold text-center">
-                        <th>#</th>
-                        <th>Question</th>
-                        <th>Your Ans</th>
-                        <th>Correct Ans</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {scorecardsData?.correct_answers.map((ques: any, index: any) => (
-                        <tr className="text-xs text-center" key={index}>
-                            <td>{index+1}</td>
-                            <td>{ques[0]}</td>
-                            <td>{ques[1]}</td>
-                            <td>{ques[2]}</td>
-                        </tr>
-                    ))}
-                    {scorecardsData?.incorrect_answers.map((ques: any, index: any) => (
-                        <tr className="text-xs text-center" key={index}>
-                            <td>{scorecardsData?.correct_answers?.length + index + 1}</td>
-                            <td>{ques[0]}</td>
-                            <td>{ques[1]}</td>
-                            <td>{ques[2]}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+        <div className="overflow-x-auto flex">
+            {
+                loading ?
+                    <div className='flex flex-row mx-auto my-2 p-5'>
+                        <h1 className='mr-2'>Loading</h1>
+                        <span className="loading loading-spinner loading-sm"></span>
+                    </div>
+                    :
+                    <table className="table">
+                        <thead>
+                            <tr className="bg-cyan-800 text-white text-xs font-bold text-center">
+                                <th>#</th>
+                                <th>Question</th>
+                                <th>Your Ans</th>
+                                <th>Correct Ans</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {scorecardsData?.correct_answers.map((ques: any, index: any) => (
+                                <tr className="text-xs text-center" key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{ques[0]}</td>
+                                    <td>{ques[1]}</td>
+                                    <td>{ques[2]}</td>
+                                </tr>
+                            ))}
+                            {scorecardsData?.incorrect_answers.map((ques: any, index: any) => (
+                                <tr className="text-xs text-center" key={index}>
+                                    <td>{scorecardsData?.correct_answers?.length + index + 1}</td>
+                                    <td>{ques[0]}</td>
+                                    <td>{ques[1]}</td>
+                                    <td>{ques[2]}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+            }
         </div>
     )
 }
